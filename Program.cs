1cfd878945d705508585f7bd04f1515090a79f4f
marcos <jkmarcosb@gmail.com>
2019-08-01 21:59:30 -0600

    re coding everything

--- Program.cs
+++ Program.cs
@@ -10,88 +10,159 @@ namespace Ox
     {
         static void Main(string[] args)
         {
-            Box boxOptions = new Box();
-            boxOptions.SetBoxTitle("Ox");
-            boxOptions.SetDate("29.07.2019");
 
-            string[] options =
+            // Set limitWidth as the potencial of base 2
+            // 2 ^ 3 = 8 characters
+
+            string renderHorizontalBorder(int start, int limitWidth, string border)
             {
-                "Calcular Cateto Adyacente",
-                "Calcular Cateto Opuesto",
-                "Calcular Hipotenusa"
-            };
+                if (start > limitWidth)
+                {
+                    return border;
+                }
 
-            boxOptions.RenderMenu(options);
+                return renderHorizontalBorder(start + 1, limitWidth, $"{border}{border}");
+            }
 
-            SelectOption(options);
-            WantContinue("Seguir calculando? ", boxOptions, options);
+            // E. G.
+
+            double regSum(double start, double end, double acc)
+            {
+                if (start > end)
+                {
+                    return acc;
+                }
+
+                return regSum(start + 1, end, acc + start);
+            }
+
+
+            List<string> Options = new List<string>();
+
+
+            Options.Add($"{regSum(2, 4, 0)}");
+
+            Console.WriteLine(renderHorizontalBorder(0, 4, "*"));
+            Console.WriteLine();
+
+            Console.WriteLine("Ox");
+            Console.WriteLine("1.2.0");
+            Console.WriteLine("31.07.2019");
+
+            Console.WriteLine();
+
+            Console.WriteLine("Escoge una opcion:");
+            Console.WriteLine();
+            Console.WriteLine("1. Calcular Cateto Adyacente");
+            Console.WriteLine("2. Calcular Cateto Opuesto");
+            Console.WriteLine("3. Calcular Cateto Hipotenusa");
+
+            Console.WriteLine();
+            Console.WriteLine(renderHorizontalBorder(0, 4, "*"));
+
+            SelectOption();
         }
 
-        // Selection 
 
-        public static void SelectOption(string[] options)
+        private static double PythagorasH(double a, double b) => Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+
+        private static double PythagorasLeg(double h, double leg) => Math.Sqrt(Math.Pow(h, 2) - Math.Pow(leg, 2));
+
+        private static double NotPositive(double leg)
         {
-            double input = Utils.AskForDouble("");
-            Pythagoras Ox = new Pythagoras();
+            if (leg < 0)
+            {
+                return 0;
+            }
+
+            return leg;
+        }
+
+        private static double NotHypotenuseBiggest(double h, double leg)
+        {
+            if (leg > h)
+            {
+                return 0;
+            }
+
+            return h;
+        }
+
+        private static double NotHypotenuseDifferent(double h, double leg)
+        {
+            if (leg == h)
+            {
+                return 0;
+            }
+
+            return h;
+        }
+
+        private static double FinallyValidation(int num, double leg)
+        {
+            Console.WriteLine(num / leg);
+
+            return leg;
+        }
+
+        private static double AskForLeg(string catetoName)
+        {
+            Console.WriteLine(catetoName);
+            var input = Console.ReadLine();
+
+            try
+            {
+                return FinallyValidation(2, NotPositive(int.Parse(input)));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return AskForLeg(catetoName);
+            }
+        }
+
+        private static double AskForH(string catetoName, double leg)
+        {
+            Console.WriteLine(catetoName);
+
+            var input = Console.ReadLine();
+
+            try
+            {
+                return FinallyValidation(2, NotHypotenuseDifferent(NotHypotenuseBiggest(NotPositive(int.Parse(input)), leg), leg));
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return AskForH(catetoName, leg);
+            }
+        }
+
+
+        private static void SelectOption()
+        {
+            int input = int.Parse(Console.ReadLine());
 
             switch (input)
             {
                 case 1:
-                    Ox.AssingLeg("Opuesto");
-                    Ox.AssingH();
-
-                    Ox.CalculateA();
-
                     Console.WriteLine();
-                    Console.WriteLine("El cateto Adyacente es: " + Ox.GetA());
-                    break;
 
-                case 2:
-                    Ox.AssingLeg("Adyacente");
-                    Ox.AssingH();
+                    double h = AskForH("Ingresa  H ", 6);
+                    Console.WriteLine(h);
 
-                    Ox.CalculateB();
+                    double b = AskForLeg("Ingresa un cateto opuesto ");
+                    Console.WriteLine(b);
 
+                    double a = PythagorasLeg(h, b);
+
+                    Console.WriteLine(a);
                     Console.WriteLine();
-                    Console.WriteLine("El cateto Opuesto es: " + Ox.GetB());
-                    break;
-
-                case 3:
-
-                    Ox.AssingA();
-                    Ox.AssingB();
-
-                    Ox.CalculateH();
-
-                    Console.WriteLine();
-                    Console.WriteLine("La Hipotenusa es: " + Ox.GetH());
                     break;
 
                 default:
-                    Console.WriteLine("Ingrese una opcion valida!");
-                    SelectOption(options);
                     break;
-            }
-        }
-
-        // Continue
-
-        public static void WantContinue(string message, Box boxOp, string[] options)
-        {
-            Console.WriteLine("");
-            Console.WriteLine(message + "(y / n)");
-
-            if (Console.ReadLine() == "n")
-            {
-                System.Environment.Exit(1);
-            }
-
-            else
-            {
-                Console.Clear();
-                boxOp.RenderMenu(options);
-                SelectOption(options);
-                WantContinue(message, boxOp, options);
             }
         }
     }
